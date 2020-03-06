c32e3f5e08088a7d66a47d9baa120dea12330c91
git-hamza <epost.hamza@gmail.com>
2020-03-06 16:35:32 +0500

    Model files added

---
+++ Webi/predict.js
@@ -0,0 +1,49 @@
+//Selecting an image
+
+$("#image-selector").change(function () {
+    let reader = new FileReader();
+    reader.onload = function () {
+        let dataURL = reader.result;
+        $("#selected-image").attr("src", dataURL);
+        $("#prediction-list").empty();
+    }
+    let file = $("#image-selector").prop("files")[0];
+    reader.readAsDataURL(file);
+}); 
+
+//Loading the model
+
+let model;
+(async function () {
+    model = await tf.loadModel("models/VGG16/model.json");
+    $(".progress-bar").hide();
+})();
+
+//Pre-processin the image
+$("#predict-button").click(async function () {
+	let image = $("#selected-image").get(0);
+	let tensor = tf.fromPixels(image)
+	    .resizeNearestNeighbor([224, 224])
+	    .toFloat()
+	    .expandDims();
+
+	//More pre-processing to be added here later
+
+	//Getting a prediction
+
+	let predictions = await model.predict(tensor).data();
+	let top5 = Array.from(predictions)
+	    .map(function (p, i) {
+	        return {
+	            probability: p,
+	            className: IMAGENET_CLASSES[i]
+	        };
+	    }).sort(function (a, b) {
+	        return b.probability - a.probability;
+	    }).slice(0, 5);
+
+	$("#prediction-list").empty();
+	top5.forEach(function (p) {
+	    $("#prediction-list").append(`<li>${p.className}: ${p.probability.toFixed(6)}</li>`);
+	});
+});
